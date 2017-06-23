f95739ba316edaf6cb920892ef9405438f24f236
Bautz, Tom <tom.bautz@axelspringer.de>
2017-06-23 14:25:58 +0200

    Small layout changes

--- src/containers/Footer/Footer.jsx
+++ src/containers/Footer/Footer.jsx
@@ -10,8 +10,8 @@ export default class Footer extends Component {
         <div className='footer__content'>
           <Socials />
           <p className='footer__links'>
-            <a href="">Impressum</a>
-            <a href="">Hinweise zum Datenschutz</a>
+            <a href='http://www.axelspringer.de/artikel/Impressum_40386.html'>Impressum</a>
+            <a href='http://www.axelspringer.de/artikel/Hinweise-zum-Datenschutz_40410.html'>Hinweise zum Datenschutz</a>
           </p>
         </div>
       </footer>
