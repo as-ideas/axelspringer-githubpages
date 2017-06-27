91bd58486d3f0ca52e660402f2967565ddc39851
Bautz, Tom <tom.bautz@axelspringer.de>
2017-06-27 10:58:53 +0200

    Split general.scss into normalize and base

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import React from 'react';
 import App from 'containers/App/App';
 
 import 'index.html';
+import 'styles/normalize.scss';
+import 'styles/base.scss';
 
 ReactDOM.render(
     <App />,
