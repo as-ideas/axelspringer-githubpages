f79cee46b7eded2718116d64dec411562f416dab
Bautz, Tom <tom.bautz@axelspringer.de>
2017-06-28 15:46:03 +0200

    edit mock data

--- server.js
+++ server.js
@@ -37,6 +37,28 @@ let stats = {
             stargazers_count: 5,
             forks_count: 3,
             watchers_count: 6
+        },
+        'bla-bla-bla': {
+            name: "bla-bla-bla",
+            owner: {
+                avatar_url: "https://avatars1.githubusercontent.com/u/6329093?v=3",
+            },
+            html_url: "https://github.com/as-ideas/crowdsource",
+            stargazers_count: 9,
+            forks_count: 2,
+            watchers_count: 3
+        }
+    },
+    'bild-de': {
+        'videoplayer': {
+            name: "videoplayer",
+            owner: {
+                avatar_url: "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e3/Logo_BILD.svg/2000px-Logo_BILD.svg.png",
+            },
+            html_url: "https://github.com/as-ideas/crowdsource",
+            stargazers_count: 12,
+            forks_count: 23,
+            watchers_count: 16
         }
     }
 }
