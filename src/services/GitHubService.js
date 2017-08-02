a1eb22458aac201cb1a7c27ac81c1110059b7302
Julian Godesa <julian.godesa@asideas.de>
2017-08-02 15:27:57 +0200

    using fetch api; using npm cors package

--- src/services/GitHubService.js
+++ src/services/GitHubService.js
@@ -2,11 +2,10 @@ import HttpService from './HttpService';
 
 class GitHubService {
     constructor() {
-        this.baseUrl = '/cached-api';
     }
 
     getStats(owner, project) {
-        return HttpService.get(this.baseUrl + '/' + owner + '/' + project);
+        return HttpService.get('/' + owner + '/' + project);
     }
 }
 
